827e80ff2f787cbdd31f8ba7824ec63b7c18e2cd
Mila Cholewicka <emilka.cholewicka@gmail.com>
2025-08-28 12:12:55 +0200

    test

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function App() {
+  //n
   return (
     <SafeAreaProvider>
       <SafeAreaView>
@@ -12,6 +13,7 @@ function App() {
             source={require("./cake.png")}
             style={{ width: 100, height: 100 }}
           />
+
           <Image
             source={{
               uri: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80",
